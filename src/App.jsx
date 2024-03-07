24235f614d1a424262ad1630420c61da318010dd
PiyushPanwarFST <piyush2002panwar@gmail.com>
2024-03-07 14:57:40 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes , Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+
+const App = () => {
+  return (
+
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+      <Route path="/contact" element={<Contact/>}></Route>
+      <Route path="/signUp" element={<SignUp/>}></Route>
+      <Route path="/signIn" element={<SignIn/>}></Route>
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar';
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      This is home page
+    </div>
+  )
+}
+
+export default Home
